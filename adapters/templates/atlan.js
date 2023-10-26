661f14304c5c364ad803df5057c076445e7b1ee8
Arpit Shukla <arpit.shukla@atlan.com>
2023-10-26 16:55:01 +0530

    iniewkmwke

--- adapters/templates/atlan.js
+++ adapters/templates/atlan.js
@@ -1,7 +1,9 @@
 import { getConnectorImage, getCertificationImage } from "../utils/index.js";
 
 export function getErrorModelNotFound(name) {
-  return `❌ Model with name **${name}** could not be found or is deleted <br><br>`;
+  return `
+  ❌ Model with name **${name}** could not be found or is deleted <br><br>
+  `;
 }
 
 export function getErrorDoesNotMaterialize(
