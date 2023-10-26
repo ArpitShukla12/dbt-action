f8c15b311788ba21635fe05d64b0f1c374d98453
Arpit Shukla <arpit.shukla@atlan.com>
2023-10-26 19:44:17 +0530

    Testing 1.1

--- adapters/integrations/gitlab-integration.js
+++ adapters/integrations/gitlab-integration.js
@@ -107,15 +107,15 @@ export default class GitLabIntegration extends IntegrationInterface {
       });
       const assetName = IS_IGNORE_MODEL_ALIAS_MATCHING ? fileName : aliasName;
       console.log("At line 88");
-      // const environments = getGitLabEnvironments();
-      // console.log("At line 90", environments);
+      const environments = getGitLabEnvironments();
+      console.log("At line 90", environments);
       let environment = null;
-      // for (const [baseBranchName, environmentName] of environments) {
-      //   if (baseBranchName === source_branch) {
-      //     environment = environmentName;
-      //     break;
-      //   }
-      // }
+      for (const [baseBranchName, environmentName] of environments) {
+        if (baseBranchName === source_branch) {
+          environment = environmentName;
+          break;
+        }
+      }
       console.log("At line 98", environment);
       const asset = await getAsset({
         //Complete
@@ -229,15 +229,15 @@ ${comments}`;
       });
       console.log("At line 202", aliasName);
       const assetName = IS_IGNORE_MODEL_ALIAS_MATCHING ? fileName : aliasName;
-      // const environments = getGitLabEnvironments();
+      const environments = getGitLabEnvironments();
 
       let environment = null;
-      // for (const [baseBranchName, environmentName] of environments) {
-      //   if (baseBranchName === source_branch) {
-      //     environment = environmentName;
-      //     break;
-      //   }
-      // }
+      for (const [baseBranchName, environmentName] of environments) {
+        if (baseBranchName === source_branch) {
+          environment = environmentName;
+          break;
+        }
+      }
 
       const asset = await getAsset({
         //Done
